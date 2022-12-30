5112b4318ac8300003f80968ccf57cd957823238
ArtMejia <arteniomejia.student@careerdevs.com>
2022-12-30 18:33:33 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Mouse2 m2 = new Mouse2();
+
+        m2.leftClick();
+        m2.rightClick();
+        m2.scrollDown();
+        m2.scrollUp();
+
+        m2.connect();
+    }
+}
